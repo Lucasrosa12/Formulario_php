36bae4eb0b79f7f59146dc3910d8ad754e68427f
Lucasrosa12 <lukasroza79@gmail.com>
2021-10-13 19:03:50 -0300

    Adicionando validação de campos do formulário

--- script.php
+++ script.php
@@ -10,7 +10,31 @@ $categorias[] = 'Idoso';
 $nome = $_POST['nome'];                 //$nome = $_GET['nome'];              MUDAR TAMBÉM NO HTML
 $idade = $_POST['idade'];               //$idade = $_GET['idade'];
 //var_dump($nome, $idade);
-//return 0;
+//return 0;  serve para n executar o codigo de baico
+if (empty($nome)) {
+	echo 'O nome está vazio';
+	return;
+}
+
+if (strlen($nome) < 3) {
+	echo 'O nome deve ter mais de 3 carcteres';
+	return;
+}
+
+if (strlen($nome) > 40) {
+	echo 'O nome é muito extenso';
+	return;
+}
+
+if (!is_numeric($idade)) {  //SE ele NÂO for um numero
+	echo 'Informe um numero para idade';
+	return;
+}
+
+if (strlen($idade) > 2) {
+	echo 'O nome é muito extenso';
+	return;
+}
 
 
 if ($idade >= 6 && $idade <= 12) {
